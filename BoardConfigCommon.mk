1858272011902de1c73cf27e5261f226c576b6f4
martincz <martincz@mokeetek.com>
2013-07-29 20:44:24 +0800

    update
    Change-Id: I19083468bc4864548270c907ac69726637c6d360

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,6 +52,7 @@ TARGET_BOARD_PLATFORM := omap4
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a9
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
